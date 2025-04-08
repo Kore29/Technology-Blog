1f63ec1c48c98bcdb07d0c0b5b50a57eb6ae612d
Arnau <a24arnpergan@inspedralbes.cat>
2025-04-08 11:58:19 +0200

    pequeño cambio

--- program/ReconocimientoFacial.py
+++ program/ReconocimientoFacial.py
@@ -34,12 +34,12 @@ while True:
         cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
         
         #EigenFace
-        #if result[1] < 5700:
-            #cv2.putText(frame,'{}'.format(imagePaths[0]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-            #cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-        #else:
-            #cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-            #cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+        if result[1] < 5700:
+            cv2.putText(frame,'{}'.format(imagePaths[0]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+        else:
+            cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
         #FisherFace
         #cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
     cv2.imshow('frame',frame)
